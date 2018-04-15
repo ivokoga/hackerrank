bc1b7b41489cd420b01e8cce63c78843d9e37f8f
ivokoga <ivo.koga@gmail.com>
2018-04-15 19:51:36 -0300

    Update kangaroo.py

--- practice/algorithms/implementation/kangaroo/kangaroo.py
+++ practice/algorithms/implementation/kangaroo/kangaroo.py
@@ -9,10 +9,10 @@ def kangaroo(x1, v1, x2, v2):
             xdiff = abs(x1-x2)
             vdiff = abs(v1-v2)
             if(xdiff % vdiff ==0):
-                return "YES"
+                return 'YES'
             else:
-                return "NO"
-    return "NO"
+                return 'NO'
+    return 'NO'
 
 x1, v1, x2, v2 = input().strip().split(' ')
 x1, v1, x2, v2 = [int(x1), int(v1), int(x2), int(v2)]
